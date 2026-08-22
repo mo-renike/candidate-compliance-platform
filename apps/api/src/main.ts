d1c6ca1aef19cec82102a211219da1ac6509fb8c
Morenike <herroyalpianist@gmail.com>
2026-08-22 19:42:55 +0100

    chore: add vercel url to cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
     credentials: true,
   });
 
